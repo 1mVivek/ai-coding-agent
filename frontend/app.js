f6702dfd0a327cc5e4bc6d213e2ec8aeb9133d21
1mVivek <gamingcodinggymrepeat@gmail.com>
2026-02-07 20:48:18 +0530

    Update app.js

--- frontend/app.js
+++ frontend/app.js
@@ -8,7 +8,7 @@ async function send() {
   chat.innerHTML += `<div class="message user">${userMsg}</div>`;
   input.value = "";
 
-  const res = await fetch("http://127.0.0.1:8000/chat", {
+  const res = await fetch("https://web-production-ba687.up.railway.app/", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ message: userMsg })
